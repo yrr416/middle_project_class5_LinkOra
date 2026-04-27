480580606662b8c23719f844b9c918cee2a993c9
jeongseok <jwjang150@protonmail.com>
2026-04-27 18:36:47 +0900

    내키로 변경

--- src/main/webapp/WEB-INF/views/map/map.jsp
+++ src/main/webapp/WEB-INF/views/map/map.jsp
@@ -131,7 +131,7 @@
 
 </main>
 
-<script type="text/javascript" src="https://dapi.kakao.com/v2/maps/sdk.js?appkey=cd1f0f4ad9dcf4879bee2531dc5a0497&libraries=services&autoload=false"></script>
+<script type="text/javascript" src="https://dapi.kakao.com/v2/maps/sdk.js?appkey=fec0b7e758b0fe490d54b99dbb1ad08c&libraries=services&autoload=false"></script>
 
 <script type="text/javascript">
   /**
--- src/main/webapp/static/js/mp_script.js
+++ src/main/webapp/static/js/mp_script.js
@@ -151,7 +151,7 @@ document.addEventListener('DOMContentLoaded', () => {
         if (!document.querySelector('script[src*="dapi.kakao.com"]')) {
             const script = document.createElement('script');
             script.type = 'text/javascript';
-            script.src = 'https://dapi.kakao.com/v2/maps/sdk.js?appkey=cd1f0f4ad9dcf4879bee2531dc5a0497&libraries=services&autoload=false';
+            script.src = 'https://dapi.kakao.com/v2/maps/sdk.js?appkey=fec0b7e758b0fe490d54b99dbb1ad08c&libraries=services&autoload=false';
             script.onload = () => { window.kakao.maps.load(initMapProcess); };
             document.head.appendChild(script);
         } else {
